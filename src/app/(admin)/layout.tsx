8231e3f610a1c044b04ea06453c764eb95996aeb
AnhNguyen <anh.nguyen@aitechasia.vn>
2023-12-22 10:21:01 +0700

    update admin layout

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,55 @@
+"use client";
+
+import {
+  AppShell,
+  Avatar,
+  Burger,
+  Flex,
+  Group,
+  Skeleton,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { MantineLogo } from "@mantinex/mantine-logo";
 import React from "react";
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const [opened, { toggle }] = useDisclosure();
   return (
-    <>
-      <p>Header Admin</p>
-      {children}
-      <p>Footer Admin</p>
-    </>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Flex px="md" h={"100%"} align={"center"}>
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+            <MantineLogo size={30} />
+          </Group>
+
+          <Flex justify={"flex-end"} className="flex-1">
+            <Avatar color="cyan" radius="xl">
+              <Text>Ad</Text>
+            </Avatar>
+          </Flex>
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
   );
 };
 
