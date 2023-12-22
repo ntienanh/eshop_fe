e8bf0b21c0ed3b2c598e350a9ad8096362fffb63
AnhNguyen <anh.nguyen@aitechasia.vn>
2023-12-22 10:03:28 +0700

    update mantine favicon eslint and some structure

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from '@mantine/core'
 import Image from 'next/image'
 
 export default function Home() {
   return (
+   <>
     <p className='text-3xl'>Hoome</p>
+    <Button variant='light'>Button</Button>
+   </>
   )
 }
