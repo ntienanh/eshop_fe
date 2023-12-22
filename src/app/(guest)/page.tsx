afed16f844a91d6ae30c1dc7fc98c94ed127109e
AnhNguyen <anh.nguyen@aitechasia.vn>
2023-12-22 16:28:33 +0700

    example dark-light at guest

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 import { useNProgress, useNProgressRouter } from "@/hooks/useNProgress";
-import { Button } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  Group,
+  useComputedColorScheme,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { notifications } from "@mantine/notifications";
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
 export default function Home() {
   useNProgress();
   const router = useNProgressRouter();
-  
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
   return (
-    <>
+    <Group>
       <p className="text-3xl">Hoome</p>
       <Button variant="light" onClick={() => router.push("/admin")}>
         move to admin
@@ -23,6 +34,20 @@ export default function Home() {
       >
         Show notification
       </Button>
-    </>
+      <ActionIcon
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="default"
+        size="lg"
+        aria-label="Toggle color scheme"
+      >
+        {colorScheme === "light" ? (
+          <IconMoonStars color='var(--mantine-color-blue-7)'/>
+        ) : (
+          <IconSun color="var(--mantine-color-yellow-4)" />
+        )}
+      </ActionIcon>
+    </Group>
   );
 }
