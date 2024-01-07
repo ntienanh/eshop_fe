045d21d2da2ab8ca1149c53463a7f4d18aabee1e
tuannguyen20198 <tuannguyen20198@gmail.com>
2024-01-07 22:37:17 +0700

    code header menu

--- src/app/(guest)/home/Header/page.tsx
+++ src/app/(guest)/home/Header/page.tsx
@@ -1,174 +1,204 @@
+import cx from 'clsx';
+import { useState } from 'react';
 import {
-    HoverCard,
-    Group,
-    Button,
-    UnstyledButton,
-    Text,
-    SimpleGrid,
-    ThemeIcon,
-    Anchor,
-    Divider,
-    Center,
-    Box,
-    Burger,
-    Drawer,
-    Collapse,
-    ScrollArea,
-    rem,
-    useMantineTheme,
-  } from '@mantine/core';
-  import { MantineLogo } from '@mantinex/mantine-logo';
-  import { useDisclosure } from '@mantine/hooks';
-  import {
-    IconNotification,
-    IconCode,
-    IconBook,
-    IconChartPie3,
-    IconFingerprint,
-    IconCoin,
-    IconChevronDown,
-  } from '@tabler/icons-react';
-  import classes from './HeaderMegaMenu.module.css';
-import mockdata from '@/utils/intex';
-  
+  Container,
+  Avatar,
+  UnstyledButton,
+  Group,
+  Text,
+  Menu,
+  Tabs,
+  Burger,
+  rem,
+  useMantineTheme,
+  ActionIcon,
+  useMantineColorScheme,
+  useComputedColorScheme,
+  Box,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+  IconLogout,
+  IconHeart,
+  IconStar,
+  IconMessage,
+  IconSettings,
+  IconPlayerPause,
+  IconTrash,
+  IconSwitchHorizontal,
+  IconChevronDown,
+  IconMoonStars,
+  IconSun,
+} from '@tabler/icons-react';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './HeaderTabs.module.css';
 
-  
-  export function Header() {
-    const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-    const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-    const theme = useMantineTheme();
-  
-    const links = mockdata.map((item) => (
-      <UnstyledButton className={classes.subLink} key={item.title}>
-        <Group wrap="nowrap" align="flex-start">
-          <ThemeIcon size={34} variant="default" radius="md">
-            <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-          </ThemeIcon>
-          <div>
-            <Text size="sm" fw={500}>
-              {item.title}
-            </Text>
-            <Text size="xs" c="dimmed">
-              {item.description}
-            </Text>
-          </div>
+const user = {
+  name: 'Jane Spoonfighter',
+  email: 'janspoon@fighter.dev',
+  image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
+};
+
+const tabs = [
+  'Home',
+  'Orders',
+  'Education',
+  'Community',
+  'Forums',
+  'Support',
+  'Account',
+  'Helpdesk',
+];
+
+export function HeaderTabs() {
+  const theme = useMantineTheme();
+  const [opened, { toggle }] = useDisclosure(false);
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+  const items = tabs.map((tab) => (
+    <Tabs.Tab value={tab} key={tab}>
+      {tab}
+    </Tabs.Tab>
+  ));
+
+  return (
+    <div className={classes.header}>
+      <Container className={classes.mainSection} size="md">
+        <Group justify="space-between">
+          <Box className='text-[28px]'>Cellina Glasses</Box>
+
+          <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+
+          <Menu
+            width={260}
+            position="bottom-end"
+            transitionProps={{ transition: 'pop-top-right' }}
+            onClose={() => setUserMenuOpened(false)}
+            onOpen={() => setUserMenuOpened(true)}
+            withinPortal
+          >
+            <Menu.Target>
+              <UnstyledButton
+                className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
+              >
+                <Group gap={7}>
+                  <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
+                  <Text fw={500} size="sm" lh={1} mr={3}>
+                    {user.name}
+                  </Text>
+                  <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                  <ActionIcon
+                  onClick={() =>
+                    setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+                  }
+                  variant="default"
+                  size="lg"
+                  aria-label="Toggle color scheme"
+                >
+                  {colorScheme === "light" ? (
+                    <IconMoonStars color='var(--mantine-color-blue-7)' />
+                  ) : (
+                    <IconSun color="var(--mantine-color-yellow-4)" />
+                  )}
+                </ActionIcon>
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item
+                leftSection={
+                  <IconHeart
+                    style={{ width: rem(16), height: rem(16) }}
+                    color={theme.colors.red[6]}
+                    stroke={1.5}
+                  />
+                }
+              >
+                Liked posts
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconStar
+                    style={{ width: rem(16), height: rem(16) }}
+                    color={theme.colors.yellow[6]}
+                    stroke={1.5}
+                  />
+                }
+              >
+                Saved posts
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconMessage
+                    style={{ width: rem(16), height: rem(16) }}
+                    color={theme.colors.blue[6]}
+                    stroke={1.5}
+                  />
+                }
+              >
+                Your comments
+              </Menu.Item>
+
+              <Menu.Label>Settings</Menu.Label>
+              <Menu.Item
+                leftSection={
+                  <IconSettings style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                }
+              >
+                Account settings
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconSwitchHorizontal style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                }
+              >
+                Change account
+              </Menu.Item>
+              <Menu.Item
+                leftSection={
+                  <IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                }
+              >
+                Logout
+              </Menu.Item>
+
+              <Menu.Divider />
+
+              <Menu.Label>Danger zone</Menu.Label>
+              <Menu.Item
+                leftSection={
+                  <IconPlayerPause style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+                }
+              >
+                Pause subscription
+              </Menu.Item>
+              <Menu.Item
+                color="red"
+                leftSection={<IconTrash style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
+              >
+                Delete account
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
         </Group>
-      </UnstyledButton>
-    ));
-  
-    return (
-      <Box pb={120}>
-        <header className={classes.header}>
-          <Group justify="space-between" h="100%">
-            <MantineLogo size={30} />
-  
-            <Group h="100%" gap={0} visibleFrom="sm">
-              <a href="#" className={classes.link}>
-                Home
-              </a>
-              <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-                <HoverCard.Target>
-                  <a href="#" className={classes.link}>
-                    <Center inline>
-                      <Box component="span" mr={5}>
-                        Features
-                      </Box>
-                      <IconChevronDown
-                        style={{ width: rem(16), height: rem(16) }}
-                        color={theme.colors.blue[6]}
-                      />
-                    </Center>
-                  </a>
-                </HoverCard.Target>
-  
-                <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                  <Group justify="space-between" px="md">
-                    <Text fw={500}>Features</Text>
-                    <Anchor href="#" fz="xs">
-                      View all
-                    </Anchor>
-                  </Group>
-  
-                  <Divider my="sm" />
-  
-                  <SimpleGrid cols={2} spacing={0}>
-                    {links}
-                  </SimpleGrid>
-  
-                  <div className={classes.dropdownFooter}>
-                    <Group justify="space-between">
-                      <div>
-                        <Text fw={500} fz="sm">
-                          Get started
-                        </Text>
-                        <Text size="xs" c="dimmed">
-                          Their food sources have decreased, and their numbers
-                        </Text>
-                      </div>
-                      <Button variant="default">Get started</Button>
-                    </Group>
-                  </div>
-                </HoverCard.Dropdown>
-              </HoverCard>
-              <a href="#" className={classes.link}>
-                Learn
-              </a>
-              <a href="#" className={classes.link}>
-                Academy
-              </a>
-            </Group>
-  
-            <Group visibleFrom="sm">
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
-  
-            <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
-          </Group>
-        </header>
-  
-        <Drawer
-          opened={drawerOpened}
-          onClose={closeDrawer}
-          size="100%"
-          padding="md"
-          title="Navigation"
-          hiddenFrom="sm"
-          zIndex={1000000}
+      </Container>
+      <Container size="md">
+        <Tabs
+          defaultValue="Home"
+          variant="outline"
+          visibleFrom="sm"
+          classNames={{
+            root: classes.tabs,
+            list: classes.tabsList,
+            tab: classes.tab,
+          }}
         >
-          <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-            <Divider my="sm" />
-  
-            <a href="#" className={classes.link}>
-              Home
-            </a>
-            <UnstyledButton className={classes.link} onClick={toggleLinks}>
-              <Center inline>
-                <Box component="span" mr={5}>
-                  Features
-                </Box>
-                <IconChevronDown
-                  style={{ width: rem(16), height: rem(16) }}
-                  color={theme.colors.blue[6]}
-                />
-              </Center>
-            </UnstyledButton>
-            <Collapse in={linksOpened}>{links}</Collapse>
-            <a href="#" className={classes.link}>
-              Learn
-            </a>
-            <a href="#" className={classes.link}>
-              Academy
-            </a>
-  
-            <Divider my="sm" />
-  
-            <Group justify="center" grow pb="xl" px="md">
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
-          </ScrollArea>
-        </Drawer>
-      </Box>
-    );
-  }
+          <Tabs.List>{items}</Tabs.List>
+        </Tabs>
+      </Container>
+    </div>
+  );
+}
--- src/app/(guest)/layout.tsx
+++ src/app/(guest)/layout.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { useMantineTheme } from "@mantine/core";
 import React from "react";
-import  { Header } from "./home/Header/page";
+import { HeaderTabs } from "./home/Header/page";
 
 const GuestLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
-      {children}
-      <p>Footer</p>
+      <HeaderTabs />
     </>
   );
 };
