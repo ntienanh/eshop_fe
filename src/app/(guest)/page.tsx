77ebf6a49647f66d83f60b0490f17de49adf4c64
tuannguyen20198 <tuannguyen20198@gmail.com>
2024-01-20 21:14:56 +0700

    done

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,69 +1,13 @@
 "use client";
-import { useNProgress, useNProgressRouter } from "@/hooks/useNProgress";
 import {
-  ActionIcon,
-  Button,
-  Grid,
   Group,
-  Image,
-  useComputedColorScheme,
-  useMantineColorScheme,
-
 } from "@mantine/core";
-import { notifications } from "@mantine/notifications";
-import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
+import Header from "./home/header";
 export default function Home() {
-  useNProgress();
-  const router = useNProgressRouter();
-  const { colorScheme, setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme("light", {
-    getInitialValueInEffect: true,
-  });
   return (
     <Group>
-<<<<<<< HEAD
-      <p className="text-4xl text-red-700">Hoome</p>
-      <Image
-        radius="md"
-        h={200}
-        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-10.png"
-      />
-      <Image
-        radius="md"
-        h={200}
-        src="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-      />
-=======
-      <p className="text-3xl">Home</p>
->>>>>>> 226bb71d714be7a8faf913a45765e8daa9769d92
-      <Button variant="light" onClick={() => router.push("/admin")}>
-        move to admin
-      </Button>
-      <Button
-        onClick={() =>
-          notifications.show({
-            title: "Default notification",
-            message: "Hey there, your code is awesome! 🤥",
-          })
-        }
-      >
-        Show notification
-      </Button>
-      <ActionIcon
-        onClick={() =>
-          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
-        }
-        variant="default"
-        size="lg"
-        aria-label="Toggle color scheme"
-      >
-        {colorScheme === "light" ? (
-          <IconMoonStars color="var(--mantine-color-blue-7)" />
-        ) : (
-          <IconSun color="var(--mantine-color-yellow-4)" />
-        )}
-      </ActionIcon>
-    </Group>
+      <Header></Header>
+    </Group >
   );
 }
